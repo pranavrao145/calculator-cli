0036beee4e0e08559ce90ddd589cc96c2bf369d6
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-10-05 16:52:28 -0400

    feat(general): first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pranavrao145/calculator-cli
+
+go 1.17
---
+++ pkg/operations/operations.go
@@ -0,0 +1,29 @@
+package operations
+
+func add() {
+	return
+}
+
+func subtract() {
+	return
+}
+
+func multiply() {
+	return
+}
+
+func divide() {
+	return
+}
+
+func exp() {
+	return
+}
+
+func log() {
+    return
+}
+
+func mod() {
+    return
+}
