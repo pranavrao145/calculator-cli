4d4261788b17c9fe1958e5d1331faea154d59f3f
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-10-05 22:08:54 -0400

    feat(operations): implemented add operation

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,7 +1,26 @@
 package operations
 
-func Add() {
-	return
+import (
+	"fmt"
+	"strconv"
+)
+
+func Add(nums ...string) (int64, bool) {
+    var sum int64
+    sum = 0
+
+    for _, num := range nums {
+        num, err := strconv.ParseInt(num, 10, 0) // attempt to convert to int 
+
+        if err != nil {
+            fmt.Println("Couldn't parse an argument!")
+            return sum, false // false for failure of function
+        }
+        
+        sum += num
+    }
+
+    return sum, true // true for success of add function
 }
 
 func Subtract() {
