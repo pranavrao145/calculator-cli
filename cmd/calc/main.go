cb28f0632a6a174068cbb4e9ccbf1a9ab1d2ad91
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-10-07 17:15:58 -0400

    feat(main): added driver code for all operations

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -1,79 +1,143 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/pranavrao145/calculator-cli/pkg/operations"
 	"github.com/urfave/cli"
 )
 
 var app = cli.NewApp() // instantiate a new app
 
 func main() {
-    // set app properties
-    app.Name = "Calculator CLI"
-    app.Usage = "A simple calculator CLI written in Golang."
-    app.Author = "cypher"
-    app.Version = "1.0.0"
+	// set app properties
+	app.Name = "Calculator CLI"
+	app.Usage = "A simple calculator CLI written in Golang."
+	app.Author = "cypher"
+	app.Version = "1.0.0"
 
+	// set up app commands
+	app.Commands = []cli.Command{
+		{
+			Name:        "add",
+			Aliases:     []string{"a"},
+			Usage:       "add {numbers (separated by space)}",
+			Description: "Adds any amount of numbers together and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Add(c.Args()...)
 
-    // set up app commands
-    app.Commands = []cli.Command{
-        {
-        	Name:        "add",
-        	Aliases:     []string{"a"},
-        	Usage:       "add {numbers (separated by space)}",
-        	Description: "Adds any amount of numbers together and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "subtract",
-        	Aliases:     []string{"sub", "s"},
-        	Usage:       "subtract [number] [number]",
-        	Description: "Subtracts the second given number from the first and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "multiply",
-        	Aliases:     []string{"m", "mult"},
-        	Usage:       "mult {numbers (separated by space)}",
-        	Description: "Multiplies any amount of numbers together and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "divide",
-        	Aliases:     []string{"div", "d"},
-        	Usage:       "divide [number] [number]",
-        	Description: "Divides the first given number by the second and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "exponent",
-        	Aliases:     []string{"exp", "e"},
-        	Usage:       "exponent [number] [number]",
-        	Description: "Calculates the first number to the exponent of the second and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "logarithm",
-        	Aliases:     []string{"log", "l"},
-        	Usage:       "logarithm [argument]",
-        	Description: "Calculates the normal logarithm (base 10) of argument given and prints out the result.",
-            Action: nil,
-        },
-        {
-        	Name:        "modulus",
-        	Aliases:     []string{"mod", "m"},
-        	Usage:       "modulus [integer] [integer]",
-        	Description: "Applies the modulus function the first given integer by the second and prints out the result.",
-            Action: nil,
-        },
-    }
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
 
-    // run app and check for errors
-    err := app.Run(os.Args)
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "subtract",
+			Aliases:     []string{"sub", "s"},
+			Usage:       "subtract [number] [number]",
+			Description: "Subtracts the second given number from the first and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Subtract(c.Args()...)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "multiply",
+			Aliases:     []string{"m", "mult"},
+			Usage:       "mult {numbers (separated by space)}",
+			Description: "Multiplies any amount of numbers together and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Multiply(c.Args()...)
+
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "divide",
+			Aliases:     []string{"div", "d"},
+			Usage:       "divide [number] [number]",
+			Description: "Divides the first given number by the second and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Divide(c.Args()...)
+
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "exponent",
+			Aliases:     []string{"exp", "e"},
+			Usage:       "exponent [number] [number]",
+			Description: "Calculates the first number to the exponent of the second and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Exp(c.Args()...)
+
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "logarithm",
+			Aliases:     []string{"log", "l"},
+			Usage:       "logarithm [argument]",
+			Description: "Calculates the normal logarithm (base 10) of argument given and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Log(c.Args()...)
+
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+		{
+			Name:        "modulus",
+			Aliases:     []string{"mod", "m"},
+			Usage:       "modulus [integer] [integer]",
+			Description: "Applies the modulus function the first given integer by the second and prints out the result.",
+			Action: func(c *cli.Context) {
+				result, status := operations.Mod(c.Args()...)
+
+				if !status {
+					fmt.Println("Error: invalid arguments.")
+					return
+				}
+
+				fmt.Println(result)
+			},
+		},
+	}
+
+	// run app and check for errors
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,7 +1,6 @@
 package operations
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 )
@@ -14,7 +13,6 @@ func Add(nums ...string) (float64, bool) {
 		num, err := strconv.ParseFloat(num, 10) // attempt to convert to int
 
 		if err != nil {
-			fmt.Println("Couldn't parse an argument!")
 			return sum, false // false for failure of function
 		}
 
@@ -28,14 +26,12 @@ func Subtract(nums ...string) (float64, bool) {
 	num1, err := strconv.ParseFloat(nums[0], 10)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
 	num2, err := strconv.ParseFloat(nums[1], 10)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
@@ -51,7 +47,6 @@ func Multiply(nums ...string) (float64, bool) {
 		num, err := strconv.ParseFloat(num, 10) // attempt to convert to int
 
 		if err != nil {
-			fmt.Println("Couldn't parse an argument!")
 			return prod, false // false for failure of function
 		}
 
@@ -65,20 +60,17 @@ func Divide(nums ...string) (float64, bool) {
 	num1, err := strconv.ParseFloat(nums[0], 0)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
 	num2, err := strconv.ParseFloat(nums[1], 0)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
 	// check for division by 0
 	if num2 == float64(0) {
-		fmt.Println("Can't divide by zero!")
 		return 0, false // false for failure of function
 	}
 
@@ -89,14 +81,12 @@ func Exp(nums ...string) (float64, bool) {
 	num1, err := strconv.ParseFloat(nums[0], 10)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
 	num2, err := strconv.ParseFloat(nums[1], 10)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
@@ -108,7 +98,6 @@ func Log(nums ...string) (float64, bool) {
 	num, err := strconv.ParseFloat(nums[0], 10)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
@@ -119,14 +108,12 @@ func Mod(nums ...string) (int64, bool) {
 	num1, err := strconv.ParseInt(nums[0], 10, 0)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
 	num2, err := strconv.ParseInt(nums[1], 10, 0)
 
 	if err != nil {
-		fmt.Println("Couldn't parse an argument!")
 		return 0, false // false for failure of function
 	}
 
